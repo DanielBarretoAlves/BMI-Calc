01c30e6866194ddeea00f7ba2c4f437b801e6a19
DanielBarretoAlves <danielbarreto559@gmail.com>
2022-02-08 16:34:28 -0300

    Idade Buttons

--- app/src/main/java/com/example/bmicalc/MainActivity.java
+++ app/src/main/java/com/example/bmicalc/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.bmicalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView ageVal;
+    private int ageNumber;
+    //TODO:Mudar o card de peso e trocar os botões por linha digitavel ou até uma tela propria para isso
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ageNumber = 0;
+        ageVal = findViewById(R.id.ageVal);
+        ageVal.setText(""+ageNumber);
+
     }
+
+    public void test(View view)
+    {
+        ageNumber++;
+        ageVal.setText(""+ageNumber);
+    }
+
+    public void setValues(View view)
+    {
+    double startValue = 0.0;
+        ageVal.setText("0");
+    }
+
 }
